6984bcaeda47a8c2fe8171e945b15fc37039e80c
Tanaka <ytnk531@users.noreply.github.com>
2021-11-16 14:18:41 +0900

    Reduce codes.

--- lib/change_within/dsl.rb
+++ lib/change_within/dsl.rb
@@ -1,5 +1,16 @@
 module ChangeWithin::Dsl
-  def change_within(timeout = 3, interval = 0.1, &block)
-    ChangeWithin::Base.new(timeout, interval, &block)
+  def change_within(timeout = 3, interval = 0.1, receiver=nil, message=nil, &block)
+    c = RSpec::Matchers::BuiltIn::Change.new(receiver, message, &block)
+    c.define_singleton_method(:change_details) do
+      @change_details ||= ChangeWithin::TimeoutChangeDetails.new(
+        matcher_name,
+        timeout,
+        interval,
+        @receiver,
+        @message,
+        &@block
+      )
+    end
+    c
   end
 end
